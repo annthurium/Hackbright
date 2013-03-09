b30895df8cba52c51528ff7d879665bef26c7ee9
Student <user@chromebox-004.(none)>
2013-03-08 18:12:48 -0800

    Exercise 5 worksgit add *

--- lettercount.py
+++ lettercount.py
@@ -1,6 +1,27 @@
 from sys import argv
+import string
 script, read_file = argv
 
 
+f = open(read_file)
+filetext = f.read()
+filetext = filetext.lower()
+f.close()
+seq = string.ascii_lowercase
 
+dictionary = dict.fromkeys(seq)
+
+for i in dictionary:
+	dictionary[i] = 0
+
+for i in filetext:
+	for j in dictionary:
+		if j == i:
+			dictionary[i] += 1
+
+sorted_dict = [x for x in dictionary.iteritems()]
+sorted_dict.sort(key=lambda x: x[0]) # sort by key
+
+for i in sorted_dict:
+	print i
 
